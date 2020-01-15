91f7167bae99da83c61a3e74cb3e97b5ed974661
neilmizzi <mizzin@hotmail.com>
2020-01-15 11:28:42 +0100

    Added code for SARSA: (Contributed also by Britt & Whitney) - New dynamic policy - New dynamic state space log - Static reward system

--- src/send_commands.py
+++ src/send_commands.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python2
 from __future__ import print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import time
 import numpy as np
-from numpy import inf
+from numpy import inf, random
 
 import robobo
 import cv2
 import sys
 import signal
+from pprint import pprint
 import prey
 
+import collections
+
 use_simulation = True
-speed = 5
+speed = 20
 dist = 500
 
 
@@ -24,8 +28,8 @@ def terminate_program(signal_number, frame):
 def main():
     signal.signal(signal.SIGINT, terminate_program)
 
-    rob = robobo.SimulationRobobo().connect(address='130.37.120.225', port=19997) if use_simulation \
-        else robobo.HardwareRobobo(camera=True).connect(address="192.168.1.6")
+    rob = robobo.SimulationRobobo('#0').connect(address='192.168.1.2', port=19997) if use_simulation \
+        else robobo.HardwareRobobo(camera=True).connect(address="192.168.43.187")
 
     if use_simulation:
         rob.play_simulation()
@@ -34,9 +38,10 @@ def main():
         #   TODO Fix Transformation
         a = np.log(np.array(rob.read_irs())) / 10
         all_sensor_info = np.array([0 if x == inf else 1 + (-x / 2) - 0.2 for x in a]) if use_simulation \
-            else np.array(rob.read_irs()) / 200
+            else np.array(np.log(rob.read_irs())) / 10
         all_sensor_info[all_sensor_info == inf] = 0
-        #print(format(all_sensor_info))
+        all_sensor_info[all_sensor_info == -inf] = 0
+        print(format(all_sensor_info[3:]))
         # [0, 1, 2, 3, 4, 5, 6, 7]
         if direction == 'front':
             return all_sensor_info[5]
@@ -50,34 +55,19 @@ def main():
             return all_sensor_info[0]
         elif direction == 'back_right':
             return all_sensor_info[2]
+        elif direction == 'all':
+            return all_sensor_info
         else:
             raise Exception('Invalid direction')
 
-    def static_policy(s):  # To be replaced by Neural Network, this is static
-        print('left: {:f}, right: {:f}'.format(get_sensor_info('front_left'),
-              get_sensor_info('front_right')))
-        if get_sensor_info('front_left') >= s \
-                and get_sensor_info('front_left') > get_sensor_info('front_right'):
-            take_action('right')
+    # safe, almost safe, not safe. combine with previous state of safe almost safe and not safe.
+    # safe to almost safe is good, almost safe to safe is okay, safe to safe is neutral
+    # s to a to r to s'.
+    # Small steps for going left or right (left or right are only rotating and straight is going forward).
+    # controller is the q values: the boundary for every sensor.
 
-        elif get_sensor_info('front_right') >= s \
-                and get_sensor_info('front_right') > get_sensor_info('front_left'):
-            take_action('left')
-        else:
-            take_action('straight')
-
-    def reward():
-        pass
-
-    def take_action(action):
-        if action == 'left':
-            move_left()
-        elif action == 'right':
-            move_right()
-        elif action == 'straight':
-            go_straight()
-        elif action == 'back':
-            move_back()
+    def move(speed1, speed2, dist):
+        rob.move(speed1, speed2, dist)
 
     def move_left():
         rob.move(-speed, speed, dist)
@@ -91,11 +81,115 @@ def main():
     def move_back():
         rob.move(-speed, -speed, dist)
 
-    # TODO Replace with SARSA
-    # Following code moves the robot
-    for i in range(500000):
-        # Q-Learning/SARSA goes here
-        take_action(static_policy(0.75))
+    boundary = [0.6, 0.8] if not use_simulation else [0.75, 0.95]
+
+    # A static collision-avoidance policy
+    def static_policy(s):
+        if get_sensor_info('front_left') >= s \
+                and get_sensor_info('front_left') > get_sensor_info('front_right'):
+            take_action('right')
+
+        elif get_sensor_info('front_right') >= s \
+                and get_sensor_info('front_right') > get_sensor_info('front_left'):
+            take_action('left')
+        else:
+            take_action('straight')
+
+    state_table = {}
+    epsilon = 0.3
+
+    def epsilon_policy(s, epsilon):
+        s = str(s)
+        # epsilon greedy
+        """"
+        ACTIONS ARE DEFINED AS FOLLOWS:
+          NUM: ACTION
+            ------------
+            0: STRAIGHT
+            1: LEFT
+            2: RIGHT
+            ------------
+        """
+
+        if epsilon > random.random():
+            return random.choice([0, 1, 2])
+        else:
+            print(state_table[s])
+            return np.argmax(state_table[s])
+
+    def take_action(action):
+        if action == 1:
+            move_left()
+        elif action == 2:
+            move_right()
+        elif action == 0:
+            go_straight()
+        # elif action == 'back':
+        #     move_back()
+
+    def get_reward(current, new):
+        if current == 0 and new == 0:
+            return 0
+        elif current == 0 and new == 1:
+            return 2
+        elif current == 1 and new == 0:
+            return 1
+        elif current == 1 and new == 1:
+            return 1
+        elif current == 1 and new == 2:
+            return -10
+        return 0
+        # TODO give negative reward for repetitions
+
+    def make_discrete(values, boundaries):
+        discrete_list = []
+        for x in values:
+            if x > boundaries[1]:
+                discrete_list.append(2)
+            elif boundaries[1] > x > boundaries[0]:
+                discrete_list.append(1)
+            elif boundaries[0] > x:
+                discrete_list.append(0)
+        return discrete_list
+
+    """
+    REINFORCEMENT LEARNING PROCESS
+    INPUT:  alpha    : learning rate
+            epsilon  : discount factor
+            episodes : no. of episodes
+            act_lim  : no. of actions robot takes before resetting space
+    """
+    def SARSA(alpha, epsilon):
+        terminate = False
+        for i in range(5):
+            current_state = make_discrete(get_sensor_info('all')[3:], boundary)
+
+            if str(current_state) not in state_table.keys():
+                state_table[str(current_state)] = [0 for _ in range(3)]
+                action = epsilon_policy(current_state, epsilon)
+            # initialise state if it doesn't exist, else retrieve the current q-value
+            while not terminate:
+                take_action(action)
+                new_state = make_discrete(get_sensor_info('all')[3:], boundary)
+                new_action = epsilon_policy(current_state, epsilon)
+                if str(new_state) not in state_table.keys():
+                    state_table[str(new_state)] = [0 for _ in range(3)]
+
+                r = get_reward(max(current_state), max(new_state))
+                print("rrrrr", r)
+
+                state_table[str(current_state)][action] += \
+                    alpha * (r + epsilon *
+                             np.array(state_table[str(new_state)][new_action])
+                             - np.array(state_table[str(current_state)][action]))
+                if max(new_state) == 2:
+                    terminate = True
+                    print("done")
+
+                current_state = new_state
+                action = new_action
+
+    SARSA(0.9, 0.9)
 
     # Following code gets an image from the camera
     # image = rob.get_image_front()
@@ -109,12 +203,16 @@ def main():
     #     print("ROB Irs: {}".format(np.log(np.array(rob.read_irs())) / 10))
     #     time.sleep(0.1)
 
-    # pause the simulation and read the collected food
-    rob.pause_simulation()
+    if use_simulation:
+        # pause the simulation and read the collected food
+        rob.pause_simulation()
 
-    # Stopping the simulation resets the environment
-    rob.stop_world()
+        # Stopping the simulation resets the environment
+        rob.stop_world()
+
+    pprint(state_table)
 
 
 if __name__ == "__main__":
     main()
+
